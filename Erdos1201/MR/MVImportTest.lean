f2a1edbe8ca6672f7b91638f417b9949c67371ad
Johan Land <johan.land@gmail.com>
2026-09-06 13:45:12 -0700

    MR: add Montgomery–Vaughan import test

---
+++ Erdos1201/MR/MVImportTest.lean
@@ -0,0 +1,9 @@
+import Zeta23.MV.Final
+
+/-! Temporary import test for the verified Montgomery–Vaughan Hilbert development. -/
+
+namespace Erdos1201.MR
+
+example : ∃ C : ℝ, 0 < C ∧ Zeta23.MVHilbert C := Zeta23.MV.mv_hilbert
+
+end Erdos1201.MR
